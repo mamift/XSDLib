0542be6d7e51d488ea4bbb6225c2521f9d418955
Muhammad Miftah <mamift@gmail.com>
2023-04-16 16:27:54 +1000

    Added polysharp - changed IListExtensions to use file-scoped namespaces.

--- XSD/Extensions/IListExtensions.cs
+++ XSD/Extensions/IListExtensions.cs
@@ -3,41 +3,40 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Xml.Schema.Linq;
 
-namespace W3C.XSD.Extensions
+namespace W3C.XSD.Extensions;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static partial class IListExtensions
 {
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
-    public static partial class IListExtensions
+    public static void AddRange<T>(this IList<T> list, IEnumerable<T> others)
+        where T: XTypedElement
     {
-        public static void AddRange<T>(this IList<T> list, IEnumerable<T> others)
-            where T: XTypedElement
-        {
-            foreach (var other in others) {
-                list.Add(other);
-            }
+        foreach (var other in others) {
+            list.Add(other);
         }
+    }
 
-        public static IList<T> Merge<T>(this IList<T> list, IEnumerable<T> others)
-            where T: XTypedElement
-        {
-            if (others == null) return list;
+    public static IList<T> Merge<T>(this IList<T> list, IEnumerable<T> others)
+        where T: XTypedElement
+    {
+        if (others == null) return list;
 
-            list.AddRange(others);
+        list.AddRange(others);
 
-            return list;
-        }
+        return list;
+    }
 
-        public static IList<T> Concatenate<T>(this IList<T> list, IEnumerable<T> others)
-            where T : XTypedElement
-        {
-            if (others == null) return list;
+    public static IList<T> Concatenate<T>(this IList<T> list, IEnumerable<T> others)
+        where T : XTypedElement
+    {
+        if (others == null) return list;
 
-            return list.Concat(others).ToList();
-        }
+        return list.Concat(others).ToList();
+    }
 
-        public static IList<T> DistinctMerge<T>(this IList<T> list, IEnumerable<T> others)
-            where T: XTypedElement
-        {
-            return list.Merge(others).Distinct(XTypedElementDeepEqualityComparer<T>.Default).ToList();
-        }
+    public static IList<T> DistinctMerge<T>(this IList<T> list, IEnumerable<T> others)
+        where T: XTypedElement
+    {
+        return list.Merge(others).Distinct(XTypedElementDeepEqualityComparer<T>.Default).ToList();
     }
 }
