e95c3c0a707389b64ea0da110f2c0fabeada8a7b
Muhammad Miftah <mamift@gmail.com>
2023-04-18 16:00:20 +1000

    Added XQualifiedName.

---
+++ XSD/XQualifedName.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+
+namespace W3C.XSD;
+
+public class XQualifedName: XmlQualifiedName
+{
+    private readonly string _nsPrefix;
+
+    public string NamespacePrefix
+    {
+        get => _nsPrefix;
+    }
+
+    public override string ToString()
+    {
+        if (_nsPrefix == null) {
+            return base.ToString();
+        }
+
+        return NamespacePrefix.Length == 0 ? Name : string.Concat(NamespacePrefix, ":", Name);
+    }
+
+    public XQualifedName(string name, string ns, string nsPrefix) : base(name, ns)
+    {
+        _nsPrefix = nsPrefix;
+    }
+}
